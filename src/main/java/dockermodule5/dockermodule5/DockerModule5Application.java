666ef6d61affda4540c4b98b06b3bbcfad62f8e7
Nachiket-01 <nachiketshinde2001@gmail.com>
2023-10-28 18:02:30 +0530

    uploading

---
+++ src/main/java/dockermodule5/dockermodule5/DockerModule5Application.java
@@ -0,0 +1,13 @@
+package dockermodule5.dockermodule5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerModule5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerModule5Application.class, args);
+	}
+
+}
---
+++ src/test/java/dockermodule5/dockermodule5/DockerModule5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dockermodule5.dockermodule5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerModule5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
